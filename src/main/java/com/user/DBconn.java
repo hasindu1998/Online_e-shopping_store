fbeb2ba5c1f3a1d30be56b3799c61272d169a53c
Moditha Marasingha <marasinghamoditha51@gmail.com>
2024-11-25 18:10:44 +0530

    DBconn done

--- src/main/java/com/user/DBconn.java
+++ src/main/java/com/user/DBconn.java
@@ -1,5 +1,25 @@
 package com.user;
 
-public class DBconn {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBconn {
+	private static String url = "jdbc:mysql://localhost:3306/ "; // add DB name to end
+	private static String username = "root";
+	private static String pwd = ""; // add your password
+	private static Connection con;
+	
+	public static Connection getConnection() {
+			
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				
+				con = DriverManager.getConnection(url,username,pwd);
+			}
+			catch(Exception e) {
+				System.out.print("Database Connection Failed!");
+			}
+			
+			return con;
+		}
 }
